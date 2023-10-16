c5b1971e622fc34cb4b1ea1d2d3d520df92a6c79
JaiTheGuy <31665724+JaiTheGuy@users.noreply.github.com>
2023-10-16 01:32:04 -0400

    Adding Python Exercises

---
+++ loopsPythonExercises/pythonLoopEx2.py
@@ -0,0 +1,21 @@
+# First Coding Session:
+# Output the following values using a while loop: 9,8,7,6,5
+########### CODE BELOW HERE #########
+
+
+
+
+
+
+
+# Second Coding Session:
+# Create a list of ingredients you'd use to make a cake (minumum of 4 elements)
+# As you iterate through the list, print out "We have that ingredient!"
+# If you come across the ingredients of either flour or sugar, print out the statement "Wait, we don't have that!" and use the keyword break
+#
+# Complete this challenge with a for loop. If you have more time, try creating a while loop solution as a bonus!
+########### CODE BELOW HERE #########
+
+
+
+
